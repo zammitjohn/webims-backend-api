c372a334e384794893bc00cee074e8bc5a4cc90e
Zammit <john.zammit@epic.com.mt>
2021-07-14 22:47:00 +0200

    Setting sql_mode at runtime for consistency

--- api/config/database.php
+++ api/config/database.php
@@ -16,6 +16,7 @@ class Database{
         try{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT); //preventing PHP from throwing PDOException! 
+            $this->conn->query("SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'");
             $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
